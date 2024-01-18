ec565feedbc493b56dd29247df35a13760a02319
Bhanu-Partap <bhanupartap25275@gmail.com>
2024-01-18 17:42:00 +0530

    completed routing

--- src/Router/index.js
+++ src/Router/index.js
@@ -3,12 +3,14 @@ import {Route, Routes, Navigate} from "react-router-dom"
 import HomePage from "../Pages/HomePage"
 import AboutPage from "../Pages/AboutPage"
 import ContactPage from "../Pages/ContactPage"
+import PublicLAyout from '../Layout/Public-Layout'
 
 export default function index() {
   return (
     <>
       <Routes>
         <Route path='/' element={<Navigate to="/home"/>}/>
+        <Route element={<PublicLAyout />}/>
         <Route path='/home' element={<HomePage />}/>
         <Route path='/about' element={<AboutPage />}/>
         <Route path='/contact' element={<ContactPage />}/>
